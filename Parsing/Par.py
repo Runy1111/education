2ec21af142374ea1b98988f083aac509cccde3b3
runy1111 <serezha.koreshkov@gmail.com>
2025-06-18 00:36:50 +0300

    Parsing

--- Parsing/Par.py
+++ Parsing/Par.py
@@ -1,6 +1,19 @@
-from Par_code import get_info
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+from Par_code import get_info, headers
 
-for i in range(1, 7):
-    url = "https://scrapingclub.com/exercise/list_basic/?page=" + str(i)
 
-    get_info(url)
+for card_url in get_info():
+
+    r = requests.get(card_url, headers=headers)
+    sleep(3)
+    soup = BeautifulSoup(r.text, "lxml")
+    data = soup.find("div", class_="my-8 w-full rounded border")
+
+    name = data.find('h3').text
+    price = data.find('h4').text
+    text = data.find('p').text
+    url_img = "https://scrapingclub.com" + data.find('img').get('src')
+
+    print(name + '\n' + price + '\n' + url_img + '\n' + text +'\n\n')
--- Parsing/Par_code.py
+++ Parsing/Par_code.py
@@ -2,23 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
+list_card_url = []
 
 headers = {"User-Agent":
                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"}
 
 
-def get_info(url):
-    sleep(3)
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
+def get_info():
+    for i in range(1, 7):
+        url = "https://scrapingclub.com/exercise/list_basic/?page=" + str(i)
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "lxml")
 
-    data = soup.find_all("div", class_="w-full rounded border")
+        data = soup.find_all("div", class_="w-full rounded border")
 
-    for now in data:
-        name = now.find('h4').text.replace('\n', '')
-        price = now.find('h5').text
-        source = "https://scrapingclub.com" + now.find('img', class_='card-img-top img-fluid').get('src')
-        print(name + '\n' + price + '\n' + source + '\n\n')
+        for now in data:
+            card_url = "https://scrapingclub.com" + now.find('a').get('href')
+            yield card_url
+
+
+
+
+
+
+        # name = now.find('h4').text.replace('\n', '')
+        # price = now.find('h5').text
+        # source = "https://scrapingclub.com" + now.find('img', class_='card-img-top img-fluid').get('src')
+        # print(name + '\n' + price + '\n' + source + '\n\n')
 
 
 
