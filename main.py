cf800f9c17739842e4fd72559863d0253f426d1b
runy1111 <serezha.koreshkov@gmail.com>
2025-06-17 21:47:23 +0300

    open url

--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-class Building:
-    year = None
-    city = None
-
-    def __init__(self, year, city):
-        self.year = year
-        self.city = city
-
-    def get_info(self):
-        print("Year:", self.year, ". City:", self.city)
+import webbrowser
 
 
-class School(Building):
-    studs = 0
-
-    def __init__(self, studs, year, city):
-        super(School, self).__init__(year, city)
-        self.studs = studs
-
-    def get_info(self):
-        super().get_info()
-        print("Students:", self.studs)
+def open_url(url):
+    webbrowser.open(url)
 
 
-class House(Building):
-    pass
-
-
-class Shop(Building):
-    pass
-
-
-school = School(150, 2007, "Moscow")
-school.get_info()
-house = House(2007, "Moscow")
-shop = Shop(2007, "Moscow")
+open_url("https://itproger.com")
