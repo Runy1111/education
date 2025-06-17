25eb12259f42dbea6caa55b3fde319a6e1b18068
runy1111 <serezha.koreshkov@gmail.com>
2025-06-18 01:00:35 +0300

    Parsing

--- Parsing/Par.py
+++ Parsing/Par.py
@@ -4,16 +4,17 @@ from time import sleep
 from Par_code import get_info, headers
 
 
-for card_url in get_info():
+def array():
+    for card_url in get_info():
 
-    r = requests.get(card_url, headers=headers)
-    sleep(3)
-    soup = BeautifulSoup(r.text, "lxml")
-    data = soup.find("div", class_="my-8 w-full rounded border")
+        r = requests.get(card_url, headers=headers)
+        sleep(1)
+        soup = BeautifulSoup(r.text, "lxml")
+        data = soup.find("div", class_="my-8 w-full rounded border")
 
-    name = data.find('h3').text
-    price = data.find('h4').text
-    text = data.find('p').text
-    url_img = "https://scrapingclub.com" + data.find('img').get('src')
+        name = data.find('h3').text
+        price = data.find('h4').text
+        text = data.find('p').text
+        url_img = "https://scrapingclub.com" + data.find('img').get('src')
 
-    print(name + '\n' + price + '\n' + url_img + '\n' + text +'\n\n')
+        yield name, price, text, url_img
