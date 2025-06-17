e0f7a013c5c6c82d91f3d5c2099220182d089a4a
runy1111 <serezha.koreshkov@gmail.com>
2025-06-18 01:01:53 +0300

    Parsing

--- Parsing/Par.py
+++ Parsing/Par.py
@@ -8,7 +8,7 @@ def array():
     for card_url in get_info():
 
         r = requests.get(card_url, headers=headers)
-        sleep(1)
+        sleep(1 )
         soup = BeautifulSoup(r.text, "lxml")
         data = soup.find("div", class_="my-8 w-full rounded border")
 
